eb61f8c2d5ed1bf00a4d25cedcf19feefb5d6b5d
alzheimeer <fogniebla@hotmail.com>
2019-07-30 16:51:31 -0500

    fix name function hx

--- convert.c
+++ convert.c
@@ -79,13 +79,47 @@ int print_h(va_list print)
 		_putchar('0');
 		return (1);
 	}
-	while (num >= 0)
+	while (num == 0)
 	{
 		k = num % 16;
 		if (k < 10)
 			b[i] = 48 + k;
 		else
 			b[i] = 87 + k;
+		num = num / 16;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+		_putchar(b[j]);
+	return (i);
+}
+
+/**
+ * print_hx - convert integer to hexadecimal
+ * @print: argument
+ * Return: count print
+ **/
+
+int print_hx(va_list print)
+{
+	int  i, j, k;
+	unsigned int num;
+	int b[100];
+
+	num = va_arg(print, unsigned int);
+	i = 0, k = 0;
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (num == 0)
+	{
+		k = num % 16;
+		if (k < 10)
+			b[i] = 48 + k;
+		else
+			b[i] = 55 + k;
 		num = num / 16;
 		i++;
 	}
